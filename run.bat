18e481798bb098656e246cc91920b6c085ae6527
Heather Cynede <heather@cynede.net>
2013-04-18 10:41:54 +0400

    NuGet works -> F# Unicode -> Failess -> ctodo -> run

--- run.bat
+++ run.bat
@@ -1,11 +1,45 @@
 @echo off
 
+cls
+SET EnableNuGetPackageRestore=true
+
+::F# Unicode
+if not exist "tools\FSharp.Unicode\tools\fsc.exe" (
+    echo Getting Custom F# Compiler with Unicode Support
+    "tools\nuget\nuget.exe" "install" "FSharp.Unicode" "-OutputDirectory" "tools" "-ExcludeVersion"
+)
+
+::Failess - FAKE with custom FSI Support and CSS EDSL Library attached
+if not exist "tools\Failess\tools\Failess.exe" (
+    echo Getting Failess build tool with CSS EDSL library
+    "tools\nuget\nuget.exe" "install" "Failess" "-OutputDirectory" "tools" "-ExcludeVersion"
+)
+
+::ctodo
+if not exist "tools\ctodo\tools\cctodo_100.exe" (
+    echo Getting light todo list management util
+	"tools\nuget\nuget.exe" "install" "ctodo" "-OutputDirectory" "tools" "-ExcludeVersion"
+)
+
 ::Env
-set compilerpath=D:\Heather\Contrib\mono\fsharp\lib\release\4.0\
-set failesspath=D:\Heather\Contrib\mono\Failess\build\
+set c=tools\FSharp.Unicode\tools\
+set f=tools\Failess\tools\
 
 ::Compile CSS
-%compilerpath%fsc.exe -o:Devil.dll --noframework --optimize+ -r:%compilerpath%FSharp.Core.dll -r:%failesspath%FailessLib.dll --target:library --warn:4 --utf8output --fullpaths devil.fs
-%failesspath%Failess.exe FSI=%compilerpath%fsi.exe devilish.fsx
+%c%fsc.exe -o:Devil.dll --noframework --optimize+ -r:%c%FSharp.Core.dll -r:%f%FailessLib.dll --target:library --warn:4 --utf8output --fullpaths devil.fs
+%f%Failess.exe FSI=%c%fsi.exe devilish.fsx
+
+::Read todo
+set todo=call todo.cmd
+rm todo.db3
+
+%todo% initdb
+%todo% set git 0
+%todo% set syncfile TODO
+%todo% sync
+
+echo +
+echo + TODO:
+%todo%
 
 pause
